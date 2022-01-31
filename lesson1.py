b08b06c6d9c65d8ed3e42930604b103604018749
kulantry <mr.kulikovandrey@mail.ru>
2022-01-31 19:47:45 +0300

    Edit code

--- lesson1.py
+++ lesson1.py
@@ -18,15 +18,30 @@ for i in range(n):
 print(array2)"""
 
 
-"Вложенный циклы"
+""""Вложенный циклы"
 "как вывести все числа составленные из цифр?"
 test = ['3', '2', '1']
 for i in range(len(test)):
     for j in range(int(len(test))):
-        print(test[i] + test[j])
+        print(test[i] + test[j])"""
 
 "Нахождение всех делителей числа"
 
+for i in range(850000, 850100):
+    min = 1000 * 1200002103
+    max = -1
+    for j in range(2, i // 2):
+        if i % j == 0 and j < min:
+            min = j
+            break
+
+    for j in range(i-1, i // 2, -1):
+        if i % j == 0 and j > max:
+            max = j
+            break
+
+    if min == 1200002103000: f = 0
+    print(i, min, max)
 
 '===================================================================================='
 
